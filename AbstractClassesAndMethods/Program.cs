1b057f6c356f943c6bebf23b9e4ea994717d0273
Steve2a <saagyemang014@st.ug.edu.gh>
2025-08-08 18:21:08 +0000

    AbstractClassesAndMethods complete

---
+++ AbstractClassesAndMethods/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace AbstractClassesAndMethods
+{
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Shape circle = new Circle(9);
+            Console.WriteLine($"Circle area: {circle.GetArea()}");
+
+            
+            Shape rectangle = new Rectangle(9, 12);
+            Console.WriteLine($"Rectangle area: {rectangle.GetArea()}");
+        }
+    }
+}
